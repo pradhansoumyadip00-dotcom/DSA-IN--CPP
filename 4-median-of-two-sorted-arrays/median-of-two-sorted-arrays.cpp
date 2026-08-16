328e7aebb1557fc595f9dcc267276cc8d4c749b6
Soumyadip Pradhan <pradhansoumyadip00@gmail.com>
2026-08-17 00:05:11 +0530

    Time: 0 ms (100.00%) | Memory: 94.9 MB (95.04%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,22 +1,55 @@
+
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n = nums1.size();
-        int m = nums2.size();
-        vector<int> c(n + m);
-        for (int i = 0; i < n; i++) {
-            c[i] = nums1[i];
+
+        // nums1 ছোট array হওয়া ভালো
+        if (nums1.size() > nums2.size()) {
+            return findMedianSortedArrays(nums2, nums1);
         }
-        for (int i = 0; i < m; i++) {
-            c[n + i] = nums2[i];
+
+        int m = nums1.size();
+        int n = nums2.size();
+
+        int low = 0;
+        int high = m;
+
+        while (low <= high) {
+
+            int cut1 = (low + high) / 2;
+
+            int cut2 = (m + n + 1) / 2 - cut1;
+
+            int left1 = (cut1 == 0) ? INT_MIN : nums1[cut1 - 1];
+            int right1 = (cut1 == m) ? INT_MAX : nums1[cut1];
+
+            int left2 = (cut2 == 0) ? INT_MIN : nums2[cut2 - 1];
+            int right2 = (cut2 == n) ? INT_MAX : nums2[cut2];
+
+            // Correct partition
+            if (left1 <= right2 && left2 <= right1) {
+
+                // Total number of elements odd
+                if ((m + n) % 2 == 1) {
+                    return max(left1, left2);
+                }
+
+                // Total number of elements even
+                return (max(left1, left2) +
+                        min(right1, right2)) / 2.0;
+            }
+
+            // cut1 বেশি ডানদিকে চলে গেছে
+            else if (left1 > right2) {
+                high = cut1 - 1;
+            }
+
+            // cut1 বেশি বাঁদিকে আছে
+            else {
+                low = cut1 + 1;
+            }
         }
-        sort(c.begin(), c.end());
-        int p = n + m;
-        if (p % 2 == 0) {
-            return (c[p / 2] + c[p / 2 - 1]) / 2.0;
-        }
-        else {
-            return c[p / 2];
-        }
+
+        return 0.0;
     }
 };
